36bd12c734470255345148234d2bc0e3cb51b8b0
kkouame <konan.kouame@soprasteria.com>
2020-05-19 23:36:45 +0200

    TypeORM integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// Environment
+import { ENV as env } from './env/env';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(env.database)],
   controllers: [AppController],
   providers: [AppService],
 })
